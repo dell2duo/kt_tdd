fc138317d4b04c4165885ad04c8a2641e4a7afc9
dell2duo <rafael.rocha.ufv@gmail.com>
2022-09-17 01:03:33 -0300

    testing if the api calls work

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
+import Manager from '../service/Manager';
+import { IBook } from '../shared/interfaces/IBook';
 
-const Home: React.FC = () => {
-  return <View />;
+const manager = new Manager();
+
+const HomeScreen: React.FC = () => {
+  const [books, setBooks] = useState([] as IBook[]);
+
+  const searchBooks = async (title: string) => {
+    const foundBooks = await manager.getBooks(title);
+    if (foundBooks) {
+      setBooks(foundBooks);
+    }
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Search for book"
+        onChangeText={text => searchBooks(text)}
+      />
+      {books.map(book => (
+        <View key={book.id}>
+          <Text>{book.volumeInfo.title}</Text>
+        </View>
+      ))}
+    </View>
+  );
 };
 
-export default Home;
+export default HomeScreen;
